a388dadab542e1051229c4b223d59c60a252d5b0
Aditya007-d <67754560+Aditya007-d@users.noreply.github.com>
2026-01-23 02:25:15 +0530

    Add files via upload

---
+++ stringOperation.py
@@ -0,0 +1,16 @@
+'''Create a string variable name with your full name. Print:
+
+The first character
+The last character
+The length of the string'''
+
+name = "Aditya Jayant ingole"
+
+print(name[0], name[-1], len(name), sep = "\n")
+
+
+# Concatenate two strings: "Hello" and "World" with a space in between.
+a = "Hello"
+b = "World"
+
+print(a + " " + b)
