134da33f09baba44edc0c8e27353a6b007b88c88
Aditya007-d <67754560+Aditya007-d@users.noreply.github.com>
2026-08-25 00:48:51 +0530

    Add files via upload

---
+++ strManipulation.py
@@ -0,0 +1,14 @@
+#Given sentence = "Coding in Python is fun", replace "fun" with "awesome" and print it.
+
+sent = "Coding in Python is fun"
+print(sent.replace("fun", "awesome").upper())
+
+
+#Find the index of the word "Python" in sentence
+
+print(sent.find("Python"))
+
+
+#Convert the entire sentence to uppercase and print it.
+
+print(sent.upper())
